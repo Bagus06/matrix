8c111d33c0ac1c4a4b69c60ab17df0e0797cdb85
Bagus06 <bagusfikrianto7@gmail.com>
2026-05-09 17:03:31 +0700

    Patch

--- application/modules/leads/views/followup.php
+++ application/modules/leads/views/followup.php
@@ -12,13 +12,11 @@
                                 <th col-type="">full_name</th>
                                 <th col-type="">phone</th>
                                 <th col-type="">email</th>
-                                <th col-type="">university_name</th>
-                                <th col-type="">course_name</th>
                                 <th col-type="">assigned_to_name</th>
-                                <th col-type="datetimerange">follow_up_date</th>
                                 <th col-type="">source_code</th>
                                 <th col-type="">status</th>
-                                <th col-type="">note</th>
+                                <th col-type="datetimerange">updated_at</th>
+                                <th col-type="datetimerange">follow_up_date</th>
                                 <td col-type="" style="text-align:right"><?= (user_ag() == "mobile") ? "<button type='button' id='btn-col-search' class='btn btn-link'><i class='fa-solid fa-magnifying-glass'></i></button>" : "" ?></td>
                             </tr>
                             <tr id="th" data-thd="1">
@@ -27,13 +25,11 @@
                                 <th>Name</th>
                                 <th>Phone</th>
                                 <th>Email</th>
-                                <th>University</th>
-                                <th>Course</th>
                                 <th>Counselor</th>
-                                <th>Follow-Up Date</th>
                                 <th>Source</th>
                                 <th>Status</th>
-                                <th>Note</th>
+                                <th>Updated Date</th>
+                                <th>Follow-Up Date</th>
                                 <th></th>
                             </tr>
                         </thead>
